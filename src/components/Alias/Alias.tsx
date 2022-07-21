d61a44e06350b23cb557cb65a029922a93b2712b
Marat Faizerakhmanov <maratismodest@gmail.com>
2022-07-21 08:57:02 +0300

    alias

--- src/components/Alias/Alias.tsx
+++ src/components/Alias/Alias.tsx
@@ -1,82 +1,88 @@
-import React, {useEffect, useMemo, useRef, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {useTimer} from 'react-timer-hook';
-import {WordProps} from "../../App";
 import {getWordsAsync, selectGame} from "../../redux/gameSlice";
 import {useAppDispatch, useAppSelector} from "../../redux/hooks";
 import {getShuffled} from "../api";
 import Button from "../Button/Button";
+import classes from './Alias.module.scss'
 
 const Alias = () => {
     const [index, setIndex] = useState(0)
-    const result = useRef(0)
+    const [correct, setCorrect] = useState(0)
+    const [skip, setSkip] = useState(0)
     const dispatch = useAppDispatch();
+
     let time = new Date();
+
     const {
         seconds,
         isRunning,
         restart,
     } = useTimer({expiryTimestamp: time, onExpire: () => console.warn('onExpire called')})
-    const {status, words: backendWords} = useAppSelector(selectGame);
-    const [words, setWords] = useState<WordProps[]>([])
-    useEffect(() => {
-        if (backendWords.length > 0) {
-            setWords(getShuffled(backendWords));
-            localStorage.setItem('words', JSON.stringify(backendWords))
-        }
 
-    }, [backendWords])
+    const {status, words} = useAppSelector(selectGame);
 
     useEffect(() => {
-        if (backendWords.length === 0) {
+        if (words.length === 0) {
             dispatch(getWordsAsync())
         }
-    }, [dispatch])
+    }, [dispatch, words.length])
 
     const shuffled = useMemo(() =>
             getShuffled(words)
         , [words])
 
 
-    if (words.length === 0) {
+    if (status === 'loading' || words.length === 0) {
         return (
             <div>Loading</div>
         )
     }
 
-    console.log('res', result.current)
     return (
-        <div style={{textAlign: 'center'}}>
-            <h1>{isRunning ? shuffled[index].tat : 'Alias'}</h1>
-            {isRunning ? 'Game' : 'Result is:' + result.current}
+        <div className={classes.alias}>
+            <span>{`Верно: ${correct}`}</span>
+            <span>{`Пропущено: ${skip}`}</span><span>{`Итог: ${correct - skip}`}</span>
+            <h1>{isRunning ? shuffled[index].tat.toUpperCase() : 'Алиас'}</h1>
+            <span>{isRunning ?  shuffled[index].rus.toUpperCase() : 'Нажми Старт для начала игры!'}</span>
             <div style={{fontSize: '100px'}}>
                 {seconds}
             </div>
-            <p>{isRunning ? 'Running' : 'Not running'}</p>
             <div style={{display: 'flex', flexDirection: 'column'}}>
-                <Button onClick={() => {
-                    result.current = result.current + 1
-                    setIndex(prevState => prevState + 1)
-                }}
-                        disabled={!isRunning}
-                        style={{marginBottom: 16}}
-                >
-                    Correct
-                </Button>
-                <Button
-                    disabled={!isRunning}
-                    onClick={() => {
+                {isRunning && (<div>
+
+                    <Button
+                        onClick={() => {
+                            setSkip(prevState => prevState + 1)
+                            setIndex(prevState => prevState + 1)
+                        }}
+                    >Пропустить</Button>
+                    &nbsp;&nbsp;&nbsp;
+                    &nbsp;&nbsp;&nbsp;
+                    <Button onClick={() => {
+                        setCorrect(prevState => prevState + 1)
                         setIndex(prevState => prevState + 1)
                     }}
-                    style={{marginBottom: 16}}>Skip</Button>
-                <Button onClick={() => {
-                    result.current = 0
-                    time = new Date();
-                    time.setSeconds(time.getSeconds() + 60);
-                    restart(time)
-                }}
-                        style={{marginBottom: 16}}
-                >Restart
-                </Button>
+                    >
+                        Верно
+                    </Button>
+                </div>)}
+                {!isRunning &&
+                    <>
+                    <Button onClick={() => {
+                        setCorrect(0)
+                        setSkip(0)
+                        time = new Date();
+                        time.setSeconds(time.getSeconds() + 60);
+                        restart(time)
+                    }}
+                    >
+                        Старт
+                    </Button>
+                    </>
+                }
+
+
             </div>
 
         </div>
